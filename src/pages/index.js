a9ff67e4f80b8b144ad6e7344dfe512df19eed2f
Wilson Herrera <wilson.wilsoon81@gmail.com>
2026-04-27 09:20:04 -0500

    Correccion de index.js y creacion de constants.example.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import Section from "../components/Section.js";
 import PopupWithImage from "../components/PopupWithImage.js";
 import PopupWithForm from "../components/PopupWithForm.js";
 import UserInfo from "../components/UserInfo.js";
-import FormValidator from "../components/FormValidator.js";
 import Api from "../components/Api.js";
 import { API } from "../utils/constants.js";
 import PopupWithConfirmation from "../components/PopupWithConfirmation.js";
---
+++ src/utils/constants.example.js
@@ -0,0 +1,4 @@
+export const API = {
+  BASE_URL: "https://around-api.es.tripleten-services.com/v1",
+  TOKEN: "COLOCA_TU_TOKEN_AQUI",
+};
